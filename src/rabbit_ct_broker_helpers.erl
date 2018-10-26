a9673fa60ee28a7c7b6f26007ea530df9f73be6e
Jean-Sébastien Pédron <jean-sebastien@rabbitmq.com>
2018-10-26 17:00:44 +0200

    Experiment around #amqqueue{} backward-compatibility
    The #amqqueue{} record is now isolated in the `amqqueue` module: all
    accesses and modifications to the record are made through this helper.
    
    See the corresponding commit in rabbitmq-common for more details.
    
    [#159298729]

--- src/rabbit_ct_broker_helpers.erl
+++ src/rabbit_ct_broker_helpers.erl
@@ -19,6 +19,7 @@
 -include_lib("common_test/include/ct.hrl").
 -include_lib("kernel/include/inet.hrl").
 -include_lib("rabbit_common/include/rabbit.hrl").
+-include_lib("rabbit/include/amqqueue.hrl").
 
 -export([
     setup_steps/0,
@@ -1379,7 +1380,9 @@ get_connection_pids(Connections) ->
       end, rabbit_networking:connections()).
 
 %% Return the PID of the given queue's supervisor.
-get_queue_sup_pid(#amqqueue { pid = QPid, name = QName }) ->
+get_queue_sup_pid(Q) when ?is_amqqueue(Q) ->
+    QName = ?amqqueue_field_name(Q),
+    QPid = ?amqqueue_field_pid(Q),
     VHost = QName#resource.virtual_host,
     {ok, AmqSup} = rabbit_amqqueue_sup_sup:find_for_vhost(VHost, node(QPid)),
     Sups = supervisor:which_children(AmqSup),
